73af0a2f97caf8c9fb7b66e51f66d4f33f0d0991
MM1901 <74110780+MM1901@users.noreply.github.com>
2020-12-19 17:22:20 +0530

    Update

--- recipesProject/App.js
+++ recipesProject/App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Dimensions} from 'react-native';
+
+const { width, height } = Dimensions.get("window");
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Bleh</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+      <View style={styles.container}>
+          <ImageBackground source = {require('./Images/FoodCamImage.jpg')} style = {styles.image1}>
+          <View style = {styles.transparant}>
+            <Text style = {[styles.textStyle , {backgroundColor: 'transparent'}]}>Use camera to scan</Text>
+          </View>
+          </ImageBackground>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          <ImageBackground source = {require('./Images/FoodTypingImage.jpg')} style = {styles.image2}>
+          <View style = {styles.transparant}>
+            <Text style = {[styles.textStyle , {backgroundColor: 'transparent'}]}>Type Ingredients</Text>
+          </View>
+          </ImageBackground>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    image1: {
+      justifyContent: 'center' , 
+      alignItems : 'center',
+      width: width,
+      height: height/3,
+      top: -20,
+    },
+
+    image2: {
+      justifyContent: 'center' , 
+      alignItems : 'center',
+      width: width,
+      height: height/3,
+      top: 40,
+    },
+
+    transparant: {
+      flex: 1,
+      justifyContent:'center',
+      alignItems : 'center',
+      alignSelf: 'stretch',
+      backgroundColor:'rgba(255 , 255 , 255 , 0.5)',
+    },
+    
+    textStyle: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: 'black',
+      fontStyle: 'italic',
+      fontFamily: 'Baskerville',
+    }
+  });
