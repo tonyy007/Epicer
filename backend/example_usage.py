752d34e61fa1d5761fa2f21860a6a0609b9536e0
wsoren1 <wsorensen@umass.edu>
2020-12-19 16:04:25 -0500

    final object done with example usage file

--- backend/example_usage.py
+++ backend/example_usage.py
@@ -1,4 +1,26 @@
 from backend.ingredientClassifier import ingredientClassifier
 
-classifier = ingredientClassifier('second_model.h5')
-print(classifier._id_image(r"validating_my_model_images\Apple\apple.jpg"))
+classifier = ingredientClassifier('second_model.h5')  # <- This file is where our keras model is stored
+
+# A classifier object has a 'cached_ingredients' field, which is mostly internally managed, however, could be modified if necessary
+print('Cached Ingredients ' + str(classifier.cached_ingredients))
+
+# The goals of this object is to handle queries, and do image processing. Let's do a basic String query
+incoming_ingredients = ['Milk', 'Potato', 'Vinegar']
+print(str(incoming_ingredients) + ' will be added to ' + str(classifier.cached_ingredients))
+print('json object: ' + str(classifier.urls_from_list(incoming_ingredients)))  # classifier defaults to save incoming new ingredients to the cache
+print("Here's the cache now!: " + str(classifier.cached_ingredients))
+
+# We can prevent things from being added to the cache like so:
+incoming_ingredients = ['Sardines']
+classifier.urls_from_list(incoming_ingredients, save_to_cache=False)
+print('Look at the lack of sardines!: ' + str(classifier.cached_ingredients))
+
+# Here's how image classification works:
+image_filepath = r'validating_my_model_images/Apple/apple.jpg'
+print("Look at these goods! " + str(classifier.urls_from_image(image_filepath)))
+print("And that ingredient was saved to the cache (should be an apple): " + str(classifier.cached_ingredients))
+
+image_filepath = r'validating_my_model_images/Apple/apple2.jpg'
+print(str(classifier.urls_from_image(image_filepath)))
+print(str(classifier.cached_ingredients))  # note that duplicate entries are avoided, and everything is stored in lower case
--- backend/ingredientClassifier.py
+++ backend/ingredientClassifier.py
@@ -1,5 +1,5 @@
 import keras
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.preprocessing.image import img_to_array, load_img
 import requests
 import json
 from functools import reduce
@@ -14,58 +14,76 @@ session = tf.compat.v1.Session(config=config)
 tf.compat.v1.keras.backend.set_session(session)
 
 
+
 class ingredientClassifier:
     def __init__(self, model_fpath):
         self.model = keras.models.load_model(model_fpath)
-        # self.cached_ingredients = ['egg', 'rice', 'cinnamon', 'flour', 'yeast']
-        self.cached_ingredients = ["potato", "apple"]
+        self.cached_ingredients = ['egg', 'rice', 'cinnamon', 'flour', 'yeast']
+        # above line is within class definition for demo purposes
 
-    def _id_image(self, fpath): # _id_image(fpath: String) -> String
+    def add_to_cache(self, ingredient):
+        if ingredient.lower() not in self.cached_ingredients:
+            self.cached_ingredients += [ingredient.lower()]
+
+    def clear_cache(self):
+        self.cached_ingredients = []
+
+    def remove_ingredient_from_cache(self, target):
+        for index, ingredient in enumerate(self.cached_ingredients):
+            if ingredient == target:
+                del self.cached_ingredients[index]
+
+    def urls_from_image(self, fpath):  # fpath: filepath of image to be classified by CNN -> JSON object
+        prediction = self._id_image(fpath)
+        self.add_to_cache(prediction) #TODO: add to cache with internal function
+
+        return self._query_cached_items()
+
+    # urls_from_list(ingredients: String[], save_to_cache: Boolean) -> JSON object
+    def urls_from_list(self, ingredients, save_to_cache=True):
+        if save_to_cache:
+            for ingredient in ingredients:
+                self.add_to_cache(ingredient)
+
+        return self._query_cached_items(ingredients)
+
+    # _id_image(fpath: String) -> String
+    def _id_image(self, fpath):
         img = load_img(
             fpath,
             target_size=(358, 358)
-                       )
-        x = img_to_array(img)
+        )
 
+        x = img_to_array(img)
         x = x.reshape((1,) + x.shape) # shape: (358, 358, 1)
 
         numerical_prediction = self.model.predict(x)[0][0]
         prediction = 'Apple' if numerical_prediction < 0.5 else 'Milk'
         return prediction
 
-    def _query_items(self, items):
+    # _query_cached_items(**additional_ingredients: String[]) -> JSON object
+    def _query_cached_items(self, additional_ingredients=None):
+        ingredients = self.cached_ingredients
+        if additional_ingredients is not None:
+            for ingredient in ingredients:
+                self.add_to_cache(ingredient)
 
-        rs = requests.Session()
+        r = requests.get('https://www.bigoven.com/recipes/search?include_ing=' + str(reduce(lambda x, acc: x + "%2C" + acc, ingredients, '')[3:]))
+        hacky_html_decode = [item.split('<a href="')[1].split('</a>')[0] for item in r.content.decode('utf8').split('<div class="recipe-tile-full')[1:]]
 
-        cookie_dict = {
-            "__utma": "244331399.879364731.1608351112.1608351112.1608389028.2",
-            "__utmc" : "244331399",
-            "__utmz": "244331399.1608389028.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)",
-            "_ga": "GA1.2.879364731.1608351112",
-            "_gid": "GA1.2.1748242046.1608389387",
-            "__atuvc": "10%7C51",
-            "category_select": "Type%20them",
-            "__utmt": "1",
-            "myIngredients": str(reduce(lambda x, acc: x + "%2C" + acc, items, '')[3:]),
-            "__utmb": "244331399.15.10.1608389028"
-        }
+        recipes = {}
 
-        for cookie in cookie_dict:
-            rs.cookies.set(cookie, cookie_dict[cookie])
+        # {
+        #   "result0": {
+        #                "url": String,
+        #                "title": String
+        #               }, ... }
 
-        print(rs.headers)
-        r = rs.post('https://www.supercook.com/dyn/results', cookies=cookie_dict)
+        for index, item in enumerate(hacky_html_decode):
+            url = item.split('">')[0]
+            title = item.split('alt="')[1].split('"/>')[0]
 
-        print(r.headers)
+            recipes[f'result{index}'] = {"url": url,
+                                         "title": title}
 
-        data = json.loads(r.content)
-        items = []
-        for item in data['results']:
-            items.append(item)
-            print(item['hash'], item['title'])
-
-        return items
-
-
-classifier = ingredientClassifier('second_model.h5')
-classifier._query_items(classifier.cached_ingredients)
+        return json.dumps(recipes)
